9607a98df0399b646f1eaffe7bc81aec699489b4
Ahmed Alghofaili <aw053692@gmail.com>
2026-07-30 02:55:08 +0300

    Update config.php

--- config.php
+++ config.php
@@ -3,4 +3,4 @@
 // احصل عليه من: https://aistudio.google.com/app/apikey
 // هذا الملف محمي عبر .htaccess بحيث لا يمكن فتحه مباشرة من المتصفح
 
-define('GEMINI_API_KEY', 'AQ.Ab8RN6JtEXGJN12XQlPUaK1pHlvuklc-lL-ObAUOK7chO5wFcA');
+define('GEMINI_API_KEY', '');
